3728198c29717897a14cc377c3bded83685fdbeb
Tobias Meyer <52013820+Hope-IT-Works@users.noreply.github.com>
2024-05-09 02:26:05 +0200

    Added script

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Bootstrap-Generator
+
 repository for bootstrap project initialization
+
+## About
+
+This PowerShell script initializes a Bootstrap project and can optionally also include Bootstrap Icons.
+
+## Usage
+
+Run the script in PowerShell:
+
+```powershell
+./bootstrap-generator.ps1
+```
+
+## Parameters
+
+- `-WithBootstrapIcons`: Add this parameter to include Bootstrap Icons.
+- `-DirectoryName`: Name for the assets directory. Default is "assets".
+- `-DestinationPath`: Directory path where the files will be saved. Default is the current path.
+
+## Workflow
+
+1. The script greets the user and verifies the destination path.
+2. It creates the directory if it does not already exist.
+3. It fetches the latest Bootstrap release and downloads the assets.
+4. The downloaded assets are extracted and the zip file is removed.
+5. The assets are moved to the destination directory and the temporary directory is removed.
+6. If the WithBootstrapIcons parameter is set, the script repeats steps 3-5 for the Bootstrap Icons.
+7. Finally, the script outputs a success message and displays the used versions of Bootstrap and, if applicable, Bootstrap Icons.
+
+## Note
+
+The developement of this script was greatly accelerated by GitHub Copilot.
---
+++ bootstrap-generator.ps1
@@ -0,0 +1,174 @@
+<# 
+.NAME
+    Bootstrap Generator
+.SYNOPSIS
+    Initializes a bootstrap project
+.DESCRIPTION
+    Initializes a bootstrap project. Can also include Bootstrap Icons.
+.EXAMPLE
+    ./bootstrap-generator.ps1
+.PARAMETER WithBootstrapIcons
+    Add parameter to include Bootstrap Icons
+.PARAMETER DirectoryName
+    Name for the assets directory
+.PARAMETER DestinationPath
+    Directory path where the files will be saved
+
+#>
+
+Param
+(
+[Parameter(HelpMessage="Add parameter to include Bootstrap Icons")]
+    [switch]
+    $WithBootstrapIcons,
+[Parameter(HelpMessage="Name for the assets directory")]
+    [string]
+    $DirectoryName = "assets",
+[Parameter(HelpMessage="Directory path where the files will be saved")]
+    [string]
+    $DestinationPath = (Get-Location).Path
+)
+
+function Get-BG_BooleanUserInput($Prompt){
+    if($Prompt.Length -gt 0){
+        $Prompt = $Prompt + " "
+    }
+    do {
+        $Result = Read-Host -Prompt ($Prompt + "(y|j/n)")
+    } while ($Result -notin "y","j","n")
+    if($Result -eq "n"){
+        $Result = $false
+    } else {
+        $Result = $true
+    }
+    return $Result
+}
+
+function Invoke-BG_Error($Message){
+    Write-Error $Message
+    exit 1
+}
+
+Write-Host "Welcome to the Bootstrap Generator"
+
+# Verify the destination path
+Write-Host "Please check if the following path is correct:"
+Write-Host ('"'+$DestinationPath+'"')
+if(!(Get-BG_BooleanUserInput -Prompt "Is this the correct path?")){
+    Invoke-BG_Error -Message "Script aborted by user"
+}
+
+# Create the directory, if it does not exist
+$BG_DownloadPath = Join-Path -Path $DestinationPath -ChildPath $DirectoryName
+if(Test-Path -Path $BG_DownloadPath){
+    Write-Host ('The directory "'+$BG_DownloadPath+'" already exists.')
+    if(!(Get-BG_BooleanUserInput -Prompt "Do you want to proceed?")){
+        Invoke-BG_Error -Message "Script aborted by user"
+    }
+    Remove-Item -Path $BG_DownloadPath -Recurse -Force
+}
+Write-Host ('Creating the directory "'+$BG_DownloadPath+'"')
+New-Item -Path $BG_DownloadPath -ItemType Directory | Out-Null
+
+# Get the latest Bootstrap release
+try {
+    Write-Host "Getting the latest Bootstrap release"
+    $Bootstrap_Release = Invoke-RestMethod -Uri "https://api.github.com/repos/twbs/bootstrap/releases/latest"
+} catch {
+    Invoke-BG_Error -Message "Failed to get the latest Bootstrap release"
+}
+$Bootstrap_Assets = $Bootstrap_Release.assets | Where-Object { $_.name -like "*dist*" -and $_.content_type -eq "application/x-zip-compressed" -and $_.name -notlike "*example*" } | Select-Object -First 1
+
+Write-Host "Found the latest Bootstrap release: $($Bootstrap_Release.tag_name)"
+Write-Host "Found the Bootstrap assets: $($Bootstrap_Assets.name)"
+
+# Download the Bootstrap assets
+$Bootstrap_FilePath = Join-Path -Path $BG_DownloadPath -ChildPath $Bootstrap_Assets.name
+try {
+    Write-Host "Downloading the Bootstrap assets"
+    Invoke-WebRequest -Uri $Bootstrap_Assets.browser_download_url -OutFile $Bootstrap_FilePath
+} catch {
+    Invoke-BG_Error -Message "Failed to download the Bootstrap assets"
+}
+
+# Extract the Bootstrap assets
+$Bootstrap_DestinationPath = Join-Path -Path $BG_DownloadPath -ChildPath "bootstrap"
+try {
+    Write-Host "Extracting the Bootstrap assets"
+    Expand-Archive -Path $Bootstrap_FilePath -DestinationPath $Bootstrap_DestinationPath
+} catch {
+    Invoke-BG_Error -Message "Failed to extract the Bootstrap assets"
+}
+
+# Remove the Bootstrap assets zip file
+Write-Host "Removing the Bootstrap assets zip file"
+Remove-Item -Path $Bootstrap_FilePath -Force
+
+# Move the Bootstrap assets
+Write-Host "Moving the Bootstrap assets"
+Get-ChildItem -Path $Bootstrap_DestinationPath -Directory -Recurse | ForEach-Object {
+    if($_.Name -eq "css" -or $_.Name -eq "js"){
+        Move-Item -Path $_.FullName -Destination $BG_DownloadPath -Force
+    }
+}
+
+# Remove the Bootstrap assets directory
+Write-Host "Removing temporary Bootstrap assets directory"
+Remove-Item -Path $Bootstrap_DestinationPath -Recurse -Force
+
+Write-Host "Bootstrap assets are now ready!"
+
+if($WithBootstrapIcons){
+    # Get the latest Bootstrap Icons release
+    try {
+        Write-Host "Getting the latest Bootstrap Icons release"
+        $Bootstrap_Icons_Release = Invoke-RestMethod -Uri "https://api.github.com/repos/twbs/icons/releases/latest"
+    } catch {
+        Invoke-BG_Error -Message "Failed to get the latest Bootstrap Icons release"
+    }
+    $Bootstrap_Icons_Assets = $Bootstrap_Icons_Release.assets | Where-Object { $_.content_type -eq "application/x-zip-compressed" -and $_.name -notlike "*example*" } | Select-Object -First 1
+    Write-Host "Found the latest Bootstrap Icons release: $($Bootstrap_Icons_Release.tag_name)"
+    Write-Host "Found the Bootstrap Icons assets: $($Bootstrap_Icons_Assets.name)"
+
+    # Download the Bootstrap Icons assets
+    $Bootstrap_Icons_FilePath = Join-Path -Path $BG_DownloadPath -ChildPath $Bootstrap_Icons_Assets.name
+    try {
+        Write-Host "Downloading the Bootstrap Icons assets"
+        Invoke-WebRequest -Uri $Bootstrap_Icons_Assets.browser_download_url -OutFile $Bootstrap_Icons_FilePath
+    } catch {
+        Invoke-BG_Error -Message "Failed to download the Bootstrap Icons assets"
+    }
+
+    # Extract the Bootstrap Icons assets
+    $Bootstrap_Icons_DestinationPath = Join-Path -Path $BG_DownloadPath -ChildPath "bootstrap-icons"
+    try {
+        Write-Host "Extracting the Bootstrap Icons assets"
+        Expand-Archive -Path $Bootstrap_Icons_FilePath -DestinationPath $Bootstrap_Icons_DestinationPath
+    } catch {
+        Invoke-BG_Error -Message "Failed to extract the Bootstrap Icons assets"
+    }
+
+    # Remove the Bootstrap Icons assets zip file
+    Write-Host "Removing the Bootstrap Icons assets zip file"
+    Remove-Item -Path $Bootstrap_Icons_FilePath -Force
+
+    # Move the Bootstrap Icons assets
+    Write-Host "Moving the Bootstrap Icons assets"
+    Get-ChildItem -Path $Bootstrap_Icons_DestinationPath -Directory -Recurse | ForEach-Object {
+        if($_.Name -eq "font"){
+            Move-Item -Path $_.FullName -Destination $BG_DownloadPath -Force
+        }
+    }
+
+    # Remove the Bootstrap Icons assets directory
+    Write-Host "Removing temporary Bootstrap Icons assets directory"
+    Remove-Item -Path $Bootstrap_Icons_DestinationPath -Recurse -Force
+
+    Write-Host "Bootstrap Icons assets are now ready!"
+}
+
+Write-Host "Bootstrap project initialization completed!"
+Write-Host "Bootstrap Version: $($Bootstrap_Release.tag_name)"
+if($WithBootstrapIcons){
+    Write-Host "Bootstrap Icons Version: $($Bootstrap_Icons_Release.tag_name)"
+}
